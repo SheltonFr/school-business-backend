3ce24cdce4bcc0298b6564a0ec560210c37352c4
Shelton Francisco <sheltonfjs@gmail.com>
2023-04-27 09:51:06 +0200

    feat: add cors dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import connectDatabase from "./database/db.js";
 import dotenv from 'dotenv'
-
+import cors from 'cors'
 
 import userRoutes from "./routes/user.route.js"
 import authRoutes from './routes/auth.route.js'
@@ -15,6 +15,8 @@ const port = process.env.PORT || 3000;
 connectDatabase();
 app.use(express.json()); // habilitar processamento de Json no projecto
 
+
+app.use(cors())
 app.use("/users", userRoutes)
 app.use("/auth", authRoutes)
 app.use("/doc", swaggerRoutes)
